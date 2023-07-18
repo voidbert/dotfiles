ddedb03f0ef6fad2998217f6a7a4c5e46557f530
voidbert <50591320+voidbert@users.noreply.github.com>
2023-07-18 15:19:58 +0000

    Add LICENSE and license headers to all scripts
    barinfo still needs license headers

--- sway/backlight.sh
+++ sway/backlight.sh
@@ -5,6 +5,21 @@
 # Tiny script to increase (./backlight.sh +p) and decrease (./backlight.sh -p)
 # backlight brightness, where p is in percent points.
 #
+# ---------------------------------- LICENSE ----------------------------------
+#
+# Copyright 2023 Humberto Gomes
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # ------------------------------- CONFIGURATION -------------------------------
 
 # Where the backlight files are located
--- vifm/logisim.sh
+++ vifm/logisim.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 # bash is needed for "$@"
 
+# ---------------------------------- LICENSE ----------------------------------
+#
+# Copyright 2023 Humberto Gomes
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ---------------------------------- SCRIPT ----------------------------------
+
 DISPLAY=:0 wmname LG3D
 DISPLAY=:0 logisim-evolution "$@"
