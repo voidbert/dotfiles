08f6d372f3e891009fc2b5232207cb0e47e6f73a
voidbert <humbertogilgomes@protonmail.com>
2024-06-22 03:27:36 +0100

    Port DuckDNS to M4

--- config.m4
+++ config.m4
@@ -1,6 +1,42 @@
 divert(-1)
 
+# --------------------------------------------- ABOUT ---------------------------------------------
+#
+# Settings for configuring the dotfiles.
+#
+# -------------------------------------------- LICENSE --------------------------------------------
+#
+# Copyright 2024 Humberto Gomes
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# --------------------------------------- GENERAL SETTINGS ----------------------------------------
+
 # Type of the system where the installation is being performed (DESKTOP, LAPTOP or SERVER)
-define(`__SYSTEM_TYPE__', `DESKTOP')
+define(`__SYSTEM_TYPE__', `SERVER')
+
+# DuckDNS domain of my home server (__SERVER_DOMAIN__.duckdns.org).
+define(`__SERVER_DOMAIN__', `________')
+
+# -------------------------------------- SERVER-ONLY SETTINGS -------------------------------------
+
+# DuckDNS's login token.
+define(`__DUCKDNS_TOKEN__', `ffffffff-ffff-ffff-ffff-ffffffffffff')
+
+# My personal phone number, that will receive Signal messages with warnings from the server.
+define(`__SIGNAL_DESTINATION__', `+351_________')
+
+# A secondary phone number, that will send Signal messages with warnings from the server.
+define(`__SIGNAL_SOURCE__', `+351_________')
 
 divert(0)dnl
---
+++ cron/duckdns.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# --------------------------------------------- ABOUT ---------------------------------------------
+#
+# A cron job to update a dynamic IP address in DuckDNS's servers.
+#
+# -------------------------------------------- LICENSE --------------------------------------------
+#
+# Copyright 2024 Humberto Gomes
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# -------------------------------------------- SCRIPT --------------------------------------------
+changequote(`«', `»')
+http_response_payload=$(mktemp)
+http_response_code=$(curl -s -w '%{http_code}' -o "$http_response_payload" \
+    'https://www.duckdns.org/update?domains=__SERVER_DOMAIN__&token=__DUCKDNS_TOKEN__&verbose=true')
+curl_exit_code=$?
+
+text_message=$(date +'DuckDNS: %Y/%m/%d %H:%M:%S -')
+return_code=1
+
+if [ "$curl_exit_code" -ne 0 ]; then
+    text_message="$text_message curl exited with $curl_exit_code. See \`man 1 curl\`."
+
+elif [ "$http_response_code" -ne 200 ]; then
+	text_message="$text_message HTTP CODE $http_response_code."
+    text_message="$text_message Response:\n$(cat "$http_response_payload")"
+
+elif [ "$(head -n1 "$http_response_payload")" != "OK" ]; then
+	text_message="$text_message $(cat "$http_response_payload")"
+
+else
+    return_code=0
+	text_message="$text_message $(tail -n1 "$http_response_payload")"
+    text_message="$text_message $(sed -n "2p" "$http_response_payload")"
+    [ "$(tail -n1 "$http_response_payload")" = "NOCHANGE" ] && unset text_message
+fi
+
+if [ -n "$text_message" ]; then
+    . ./signal.sh
+    echo "$text_message" 1>&2
+    second_log_message=$(send_signal_message "$text_message")
+    if [ $? -ne 0 ]; then
+        echo "$second_log_message" 1>&2
+        return_code=0
+    fi
+fi
+
+rm "$http_response_payload"
+exit "$return_code"
